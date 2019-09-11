97264a7b2ac5915060d56ed2ddd3b344b48f7fbc
Atanas Arshinkov <a.arshinkov97@gmail.com>
2019-09-11 20:09:48 +0300

    Added more test

--- src/test/java/com/safb/tdd/test/controllers/HomeControllerTest.java
+++ src/test/java/com/safb/tdd/test/controllers/HomeControllerTest.java
@@ -2,18 +2,19 @@ package com.safb.tdd.test.controllers;
 
 import com.safb.tdd.config.AppConfig;
 import com.safb.tdd.config.ViewConfig;
-import com.safb.tdd.controllers.*;
-import org.junit.*;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.*;
-import org.springframework.test.web.servlet.request.*;
-import org.springframework.test.web.servlet.result.*;
-import org.springframework.test.web.servlet.setup.*;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -22,16 +23,12 @@ import org.springframework.web.context.WebApplicationContext;
 public class HomeControllerTest {
 
     @Autowired
-    private ViewConfig viewConfig;
-
-    @Autowired
     private WebApplicationContext wac;
 
     private MockMvc mock;
 
     @Before
     public void setup() {
-
         this.mock = MockMvcBuilders.webAppContextSetup(this.wac).build();
     }
 
--- src/test/java/com/safb/tdd/test/controllers/LoginControllerTest.java
+++ src/test/java/com/safb/tdd/test/controllers/LoginControllerTest.java
@@ -1,19 +1,35 @@
 package com.safb.tdd.test.controllers;
 
-import com.safb.tdd.controllers.*;
-import org.junit.*;
-import org.springframework.test.web.servlet.*;
-import org.springframework.test.web.servlet.request.*;
-import org.springframework.test.web.servlet.result.*;
-import org.springframework.test.web.servlet.setup.*;
+import com.safb.tdd.config.AppConfig;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = AppConfig.class)
+@WebAppConfiguration
 public class LoginControllerTest {
+
+    @Autowired
+    private WebApplicationContext wac;
 
     private MockMvc mock;
 
     @Before
     public void setup() {
-        this.mock = MockMvcBuilders.standaloneSetup(new LoginController()).build();
+        this.mock = MockMvcBuilders.webAppContextSetup(this.wac).build();
     }
 
     @Test
@@ -30,7 +46,6 @@ public class LoginControllerTest {
                 .andExpect(view)
                 .andExpect(globalMenuAttribute)
                 .andExpect(submenuAttribute)
-                .andDo(MockMvcResultHandlers.print())
                 .andReturn();
     }
 }
